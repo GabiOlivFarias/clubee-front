db303b04bd53b29ee4a385def7d78c5c0a36b6b6
Gabi Farias <you@example.com>
2025-09-29 04:20:32 -0400

    [NEW] first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', 
+  // 💡 CORREÇÃO: Remova ou comente a linha 'base' para usar a raiz (/)
+  // base: '/clubee-front/', 
   plugins: [react()],
 })
